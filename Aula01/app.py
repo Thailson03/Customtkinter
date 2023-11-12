a05c31591ddb0482ca8a4d5909d06b3428443b93
Thailson Diogo <thailson.diogo7@gmail.com>
2023-11-11 23:45:57 -0300

    Segundo dia estudando Customtkinter
    Terminei a aula 03, 04, 05, 06. E estou estudando no momento a aula 07.

--- Aula01/app.py
+++ Aula01/app.py
@@ -1,10 +1,10 @@
-import customtkinter as ctk#Importando a biblioteca
+import customtkinter as ctk #Importando a biblioteca
 
 janela = ctk.CTk() #Criar a nossa janela
 
 #janela._set_appearance_mode("dark")
 
-btn = ctk.CTkButton(janela, text="Ola", fg_color="#900090", hover_color="#611f61")
+btn = ctk.CTkButton(janela, text="Ola")
 btn.pack()
 
 janela.mainloop()
