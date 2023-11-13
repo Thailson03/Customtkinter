436704c3a0de09818131820c1f15d6e84a3b81e3
Thailson Diogo <thailson.diogo7@gmail.com>
2023-11-13 01:33:54 -0300

    Terceiro dia estudando Customtkinter
    Terminei a aula 07, 08, 09, 10, 11 e 12.

--- Aula07/main.py
+++ Aula07/main.py
@@ -10,7 +10,7 @@ janela.minsize(width=500, height=250)
 janela.resizable(width=False, height=False)
 
 #Aula 07 = TextBox (Caixa de texto no Customtkinter)
-textbox = ctk.CTkTextbox(janela, width=300, height=350)
+textbox = ctk.CTkTextbox(janela, width=300, height=350, scrollbar_button_color="green", scrollbar_button_hover_color="red", border_color="red", border_width=2, corner_radius=15, fg_color="teal", text_color="white", border_spacing=20, activate_scrollbars=True)
 textbox.pack()
 
 textbox.insert("0.0", "Título do seu texto\n\n" + "Ola dev, estou aqui programando interfaces graficas com customtkinter no canal set-escola de programacao e esta sendo uma boa.\n\n"*20)
